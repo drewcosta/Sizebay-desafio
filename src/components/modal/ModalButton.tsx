b00cfcc87da7e50a69129227ef565b699b0b0d7b
Andrew Costa <andrewcosta.contato@gmail.com>
2023-12-08 15:12:25 -0300

    style: refactor button border-radius styles

--- src/components/modal/ModalButton.tsx
+++ src/components/modal/ModalButton.tsx
@@ -19,9 +19,12 @@ export const Button = styled.button<Props>`
   padding: 15px;
   background: ${(props) => props.background};
   border: none;
-  border-top-right-radius: 4px;
-  border-bottom-right-radius: 4px;
   cursor: pointer;
+  
+  & + button{
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+  }
 
   & > svg{
     color: white;
