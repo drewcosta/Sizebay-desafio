894707b10da766e61b64fe852a09b1573c88c0cb
Andrew Costa <andrewcosta.contato@gmail.com>
2023-12-16 20:28:23 -0300

    feat: create hook useCrud to tasks manager

---
+++ src/hooks/useTaskCrud.tsx
@@ -0,0 +1,35 @@
+import { Task } from "../types/Task";
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useCrud = () => {
+  const {value: tasks, updateLocalStorage} = useLocalStorage<Task[]>('tasks-list', []);
+
+  const handleCreateTask = (newTask: Task) => {
+    const updateTasks = [...tasks, newTask];
+    return updateLocalStorage(updateTasks);
+  }
+
+  const handleEditTask = (editTask: Task) => {
+    const updateTasks = tasks.map(task => task.id === editTask.id ? editTask : task)
+    return updateLocalStorage(updateTasks);
+  }
+
+  const handleRemoveTask = (removeTask: Task) => {
+    const updateTasks = tasks.filter(task => task.id !== removeTask.id);
+    return updateLocalStorage(updateTasks);
+  }
+
+  const handleConfirmTask = (confirmTask: Task) => {
+    const updateTasks = tasks.map(task => task.id === confirmTask.id ? confirmTask : task);
+    return updateLocalStorage(updateTasks);
+  }
+
+  return {
+    tasks,
+    handleCreateTask,
+    handleEditTask,
+    handleRemoveTask, 
+    handleConfirmTask
+  }
+
+}
