8c59935e0195188c0b7b4a15266f8cb2a9ac8bf5
Andrew Costa <andrewcosta.contato@gmail.com>
2024-03-06 22:12:03 -0300

    delete unnecessary files

--- src/utils/FilterStatus.ts
+++
@@ -1,7 +0,0 @@
-import { TaskStatus } from "../types/TaskStatus"
-
-export const getTaskStatus = (status: TaskStatus) => {
-  if(status === TaskStatus.Done) return 'done';
-  if(status === TaskStatus.Pending) return 'pending';
-  return '';
-}
