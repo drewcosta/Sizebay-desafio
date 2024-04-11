487ceadc6f0947e09e047a93fc2bb45b034237e1
Andrew Costa <andrewcosta.contato@gmail.com>
2024-04-11 00:04:43 -0300

    feat: import animation for 100% completed tasks

--- src/components/modal/ProgressBar/ProgressBar.styles.ts
+++ src/components/modal/ProgressBar/ProgressBar.styles.ts
@@ -1,7 +1,11 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import { ProgressBarStyles } from "./IProgressBar"
 
 export const ProgressBar = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
   width: 100%;
   height: 24px;
   background: ${({ theme }) => theme.colors.grayLight};
@@ -12,4 +16,26 @@ export const ProgressBarPercentage = styled.div<ProgressBarStyles>`
   height: 24px;
   background: ${({ theme }) => theme.colors.greenTaskBar};
   border-radius: 4px;
+`
+
+export const Animation = styled.div`
+  ${({ theme }) =>
+    css`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      bottom: 1%;
+      left: 75%;
+      width: 16vw;
+      height: 21.7vh;
+
+      @media (max-width: ${theme.breakpoints.tablets}) {
+        bottom: 0;
+        left: 85%;
+        width: 21vw;
+        height: 11.7vh;
+      }
+    `
+  }
 `
--- src/components/modal/ProgressBar/ProgressBar.tsx
+++ src/components/modal/ProgressBar/ProgressBar.tsx
@@ -1,10 +1,15 @@
 import * as S from './ProgressBar.styles';
 import { ProgressBarProps } from "./IProgressBar";
+import { LottieAnimation } from '../LottieAnimation';
+import Confetti from '../../../assets/Confetti.json';
 
-export function ModalProgressBar({ percentage }: ProgressBarProps){
-  return(
+export function ModalProgressBar({ percentage }: ProgressBarProps) {
+  return (
     <S.ProgressBar>
       <S.ProgressBarPercentage width={percentage} />
-    </S.ProgressBar>
+      <S.Animation>
+        {percentage === 100 && <LottieAnimation animation={Confetti} />}
+      </S.Animation>
+    </S.ProgressBar >
   )
 }
