a76b77485c69d90eb5c86c2d276c4e54d1f5eb89
Andrew Costa <andrewcosta.contato@gmail.com>
2024-04-15 21:56:58 -0300

    feat: show result according to status

--- src/components/Modal/TaskList/NoResults/INoResults.ts
+++ src/components/Modal/TaskList/NoResults/INoResults.ts
@@ -1,3 +1,4 @@
 export interface NoResultsProps{
   removeFilters: () => void;
+  statusResult: string;
 }
--- src/components/Modal/TaskList/NoResults/NoResults.tsx
+++ src/components/Modal/TaskList/NoResults/NoResults.tsx
@@ -1,11 +1,30 @@
 import * as S from './NoResults.styles'
 import { NoResultsProps } from './INoResults';
 
-export function NoResults({ removeFilters }: NoResultsProps) {
+export function NoResults({ removeFilters, statusResult }: NoResultsProps) {
+
+  if (statusResult === 'done') {
+    return (
+      <S.Container>
+        <p> There are no items marked as done. <span onClick={removeFilters}>Clear the filter here</span> to see all items.</p>
+      </S.Container>
+    )
+  }
+
+  if (statusResult === 'pending') {
+    return (
+      <S.Container>
+        <p> There are no items marked as pending. <span onClick={removeFilters}>Clear the filter here</span> to see all items.</p>
+      </S.Container>
+    )
+  }
+
   return (
     <S.Container>
-      <p> Nenhum item encontrado. <span onClick={removeFilters}>Remova o filtro aqui</span> para ver todos os itens.</p>
+      <p> You search found no results. <span onClick={removeFilters}>Clear the filter here</span> to see all items.</p>
     </S.Container>
-  );
+  )
+
+
 }
 
