eb0daea40ef54daf92dfc9fdc04588d5e3947451
Andrew Costa <andrewcosta.contato@gmail.com>
2024-04-17 18:00:36 -0300

    feat: add task operations to context

--- src/types/IFilterContext.ts
+++
@@ -1,7 +0,0 @@
-export interface IFilterContext{
-  currentStatus: string,
-  searchTask: string,
-  setCurrentStatus: (value: string) => void,
-  setSearchTask: (value: string) => void,
-  removeFilters: () => void,
-}
